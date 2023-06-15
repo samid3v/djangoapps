2639a9a9df4a2b3ba86a032230c43616c738a464
samid3v <swanyoike154@gmail.com>
2023-06-15 15:38:25 +0300

    reviews update

--- books/admin.py
+++ books/admin.py
@@ -1,8 +1,15 @@
 from django.contrib import admin
-from .models import Book
+from .models import Book, Reviews
 
 # Register your models here.
+class ReviewInline(admin.TabularInline):
+    model = Reviews
+
 class BookAdmin(admin.ModelAdmin):
+    inlines = [
+            ReviewInline,
+        ]
+
     list_display = ['title', 'author', 'price']
 
 admin.site.register(Book, BookAdmin)
